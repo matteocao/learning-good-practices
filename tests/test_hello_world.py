76de5d5eb272ce62845d9207b933747af0207b7b
matteocao <matteocao@gmail.com>
2019-09-08 02:47:50 +0200

    Update test_hello_world.py

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -4,7 +4,13 @@ from functions import hello_world
 
 
 def test_hello_world(capsys):
-    """Correct name argument prints"""
+    """Correct `name` argument prints"""
     hello_world.hello_world("Jill")
     captured = capsys.readouterr()
     assert "Jill" in captured.out
+
+
+def test_hello_world_exc(capsys):
+    """Correct type for `name`"""
+    with pytest.raises(TypeError):
+        assert hello_world.hello_world(1)
