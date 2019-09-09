77231b020ad1980352e76de1ed69bc49748f1834
matteocao <matteocao@gmail.com>
2019-09-09 08:55:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
+with open("README.rst", "r") as readme_file:
     readme = readme_file.read()
 
 requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
@@ -10,7 +10,8 @@ setup(
     version="0.0.1",
     author="Matteo Caorsi",
     author_email="matteocao@gmail.com",
-    description="A package to.. learn to set up a git repo",
+    license="Apache License 2.0",
+    description="A package to learn to set up a git repo",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/matteocao/learning-good-practices",
@@ -18,6 +19,6 @@ setup(
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: Apache Software License",
     ],
 )
