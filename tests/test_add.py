8723953a0785414ca0d66e011b978248e207443a
matteocao <matteocao@gmail.com>
2019-09-12 01:05:45 +0200

    Update test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,6 +1,6 @@
 """Tests for `hello_world` method. Catch from capsys."""
 import pytest
-from functions import adds
+from functions import add
 
 def test_add():
-    assert adds.add(2, 3) == 5
+    assert add.add(2, 3) == 5
