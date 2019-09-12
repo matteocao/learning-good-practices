6f4eeede9f2db2d4b0f051eda6c44bb984a979eb
matteocao <matteocao@gmail.com>
2019-09-12 02:17:52 +0200

    Delete test_add.py

--- tests/test_add.py
+++
@@ -1,7 +0,0 @@
-"""Tests for `hello_world` method. Catch from capsys."""
-import pytest
-import functions.addition #in here, use the name is inside PYBIND11_MODULE()
-
-
-def test_add():
-    assert addition.add(2, 3) == 5
