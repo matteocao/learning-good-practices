18001cda9e5a325cd9898afb76c084a48cb5c672
matteocao <matteocao@gmail.com>
2019-09-12 02:24:21 +0200

    Create main.cpp

---
+++ functions/main.cpp
@@ -0,0 +1,18 @@
+#include <pybind11/pybind11.h>
+
+
+int add(int i, int j) {
+    return i + j;
+}
+
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(addition, m) {
+    // optional module docstring
+    m.doc() = "pybind11 example plugin";
+    
+    // define add function
+    m.def("add", &add, "A function which adds two numbers");
+    
+}
