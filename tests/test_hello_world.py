c802db13ffd0278446e47cb618d2be22160d2c08
matteocao <38946719+matteocao@users.noreply.github.com>
2019-09-13 11:13:51 +0200

    Update test_hello_world.py

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -5,7 +5,7 @@ from functions import hello_world
 
 def test_hello_world(capsys):
     """Correct `name` argument prints"""
-    hello_world.hello_world("Jill")
+    hello_world("Jill")
     captured = capsys.readouterr()
     assert "Jill" in captured.out
 
